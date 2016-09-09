0408c92ba84699289a58569e7c7cee16401710e0
Daniel McCoy <danieljmccoy85@gmail.com>
2016-09-09 01:37:56 -0700

    added play and pause functionality

--- portfolioTemplate.js
+++ portfolioTemplate.js
@@ -1,22 +1,37 @@
 // create modules or iife or something similar upon organizing files, not sure exact structure yet
 
-// video player logic
-var maxProg, mmedia, play, bar, progress, mute, voulume, loop;
+(function() {
+  // video player logic
+  var maxProg, mmedia, play, bar, progress, mute, voulume, loop;
 
-function init(){
+  function init() {
 
-  maxProg = 450; // max size of progress bar
-  mmedia = document.getElementById("media");
-  play = document.getElementById("play");
-  bar = document.getElementById("bar");
-  progress = document.getElementById("progress");
-  mute = document.getElementById("mute");
-  volume = document.getElementById("volume");
+    maxProg = 450; // max size of progress bar
+    mmedia = document.getElementById("media");
+    play = document.getElementById("play");
+    bar = document.getElementById("bar");
+    progress = document.getElementById("progress");
+    mute = document.getElementById("mute");
+    volume = document.getElementById("volume");
 
 
-  play.addEventListener("click", push);
-  mute.addEventListener("click", sound);
-  bar.addEventListener("click", move);
-  volume.addEventListener("change", level);
+    play.addEventListener("click", push);
+    mute.addEventListener("click", sound);
+    bar.addEventListener("click", move);
+    volume.addEventListener("change", level);
 
-}
+  }
+
+  function push() {
+    if (!mmedia.paused && !media.ended) {
+      mmedia.pause();
+      play.value = "Play";
+      clearInterval(loop);
+    } else {
+      mmedia.play();
+      play.value = "Pause";
+      loop = setInterval(status, 1000);
+    }
+  }
+  
+})
